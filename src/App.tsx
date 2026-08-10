61f4a3c0d2887cc6f0fae784dae782ffa9752b8d
whatisupwithme <90317371+whatisupwithme@users.noreply.github.com>
2026-08-11 00:30:17 +0530

    Delete src directory

--- src/App.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Studio } from './components/Studio';
-
-export function App() {
-  return <Studio />;
-}
--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import "./index.css";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { App } from "./App";
-
-const rootEl = document.getElementById("root");
-if (rootEl) {
-  ReactDOM.createRoot(rootEl).render(<App />);
-}
